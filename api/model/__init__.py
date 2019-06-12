4f4ceb69331d83e3c90fa2226c27fa7f6e06e7a6
choichanhwan <chanhwan@elicer.com>
2019-06-12 20:40:37 +0900

    feat: set basic

---
+++ api/model/__init__.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config
+
+
+db = SQLAlchemy()
+
+
+def init_extensions(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}?charset=utf8'.format(
+                                            config.DB_USER, config.DB_PASSWORD, config.DB_ADDRESS, config.DB_NAME)
+    db.init_app(app)
+    return db
+
+
