645cb0a78deac1f87e71da1c5da4e15c40e45d16
Chanhwan Choi <choich@choich.space>
2019-06-21 10:07:53 +0900

    Feature/add qa (#20)
    * fix: qa api

--- api/resource/qa.py
+++ api/resource/qa.py
@@ -19,24 +19,30 @@ def max_length(max_length):
     return validate
 
 
+def dt_to_str(dt):
+    return dt.strftime('%Y년 %m월%d일 %H:%M')
+
+
 qa_list_post_parser = reqparse.RequestParser()
 qa_list_post_parser.add_argument('name', type=max_length(16), required=True, location='form')
 qa_list_post_parser.add_argument('email', type=max_length(32), required=True, location='form')
-qa_list_post_parser.add_argument('title', type=max_length(256), required=True, location='form')
-qa_list_post_parser.add_argument('context', type=str, required=True, location='form')
+qa_list_post_parser.add_argument('question', type=str, required=True, location='form')
 
 
 qa_list_get_parser = reqparse.RequestParser()
 qa_list_get_parser.add_argument('count', type=int, required=True)
 qa_list_get_parser.add_argument('offset', type=int, required=True)
 
+qa_patch_parser = reqparse.RequestParser()
+qa_patch_parser.add_argument('answer', type=str, required=True, location='form')
+
 qa_fields = {
     'id': fields.Integer,
     'name': fields.String,
     'email': fields.String,
-    'title': fields.String,
-    'context': fields.String,
-    'create_datetime': fields.DateTime
+    'question': fields.String,
+    'answer': fields.String,
+    'create_datetime': fields.String
 }
 
 qa_list_fields = {'qa': fields.Nested(qa_fields)}
@@ -50,8 +56,7 @@ class QaListResource(Resource):
         qa = QaModel(
             name=args.name,
             email=args.email,
-            title=args.title,
-            context=args.context,
+            question=args.question,
             create_datetime=datetime.now(timezone('Asia/Seoul'))
         )
 
@@ -71,7 +76,12 @@ class QaListResource(Resource):
             .offset(args.offset) \
             .all()
 
-        return {'qa': qa_list}
+        qa_list_str = []
+        for i in qa_list:
+            i.create_datetime = dt_to_str(i.create_datetime)
+            qa_list_str.append(i)
+
+        return {'qa': qa_list_str}
 
 
 class QaResource(Resource):
@@ -86,4 +96,23 @@ class QaResource(Resource):
         if qa is None:
             abort(404, 'The article is not found.')
 
+        qa.create_datetime = dt_to_str(qa.create_datetime)
+
         return qa
+
+    def patch(self, id):
+        args = qa_patch_parser.parse_args()
+
+        qa = db.session \
+            .query(QaModel) \
+            .filter(QaModel.id == id) \
+            .first()
+
+        if qa is None:
+            abort(404, 'The article is not found.')
+
+        qa.answer = args.answer
+
+        db.session.commit()
+
+        return {'status': 'ok'}
